5a623952eb85d3e67f9f30bf05b54776c2749d2c
hugues <ghugues@netc.fr>
2019-10-27 11:59:25 +0100

    add 404.md

---
+++ 404.js
@@ -0,0 +1,14 @@
+(()=>{
+	var u = document.URL.replace(/.*:\/\/.*?\/(.*)\??.*/, "$1").split("/");
+	var last = u.pop();
+	var all = '<a href="/">/</a>' ;
+	var root = "" ;
+	for (let e of u) {
+		root += "/" + e ;
+		all += `<a href="${root}">${e}/</a>`
+	}
+	if (last) {
+		all += `<a href="${root}/${last}">${last}</a>`
+	}
+	document.getElementById("404").innerHTML = all ;
+})();
