b1b164788868a229f1383bc54a4704fc116b480b
hugues <ghugues@netc.fr>
2019-10-27 11:16:43 +0100

    add list the docs repos

---
+++ index.md
@@ -0,0 +1,16 @@
+---
+title: Hugues GUILLEUS
+---
+
+<style>
+	h1:first-of-type{ display: none; }
+	hgroup{
+		font-weight: bold;
+	}
+</style>
+
+# Hugues GUILLEUS
+
+<h2>Repository</h2>
+<ul id="lsRepo"></ul>
+<script src="ls.js"></script>
---
+++ ls.js
@@ -0,0 +1,38 @@
+// 2019 GUILLEUS Hugues
+
+(async ()=>{
+	try {
+		var repos = await getRepos();
+		var ls = document.getElementById("lsRepo")
+		for (let repo of repos) {
+			if (typeof repo.homepage != "string") {
+				continue ;
+			}
+			if (repo.homepage.indexOf("https://huguesguilleus.github.io/") != 0) {
+				continue ;
+			}
+			ls.insertAdjacentHTML("beforeend", `<li>
+				<hgroup>${repo.name}</hgroup>
+				<a href="${repo.homepage}">${repo.homepage}</a>
+				${
+					repo.description ? `<p>${repo.description}</p>` : ""
+				}
+			</li>` )
+		}
+	} catch (e) {
+		console.log(e);
+	}
+})();
+
+// Get the list of the repository
+function getRepos() {
+	var req = new XMLHttpRequest();
+	req.open("get", "https://api.github.com/users/HuguesGuilleus/repos", true)
+	req.send();
+	return new Promise(function(resolve, reject) {
+		req.addEventListener("load", event=>{
+			resolve(JSON.parse(req.responseText));
+		}, false);
+		req.addEventListener("error", reject, false);
+	});
+}
